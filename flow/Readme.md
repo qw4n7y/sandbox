fe6f3fbd350ed852c66dec3f0609abf4ad6069d9
Yury Samarychau <qw4n7y@gmail.com>
2018-05-24 18:53:34 +0300

    flow: basic

---
+++ flow/Readme.md
@@ -0,0 +1,5 @@
+### Dao
+
+yarn run flow init
+
+yarn run flow status
---
+++ flow/src/index.js
@@ -0,0 +1,7 @@
+// @flow
+
+function sum(a: number, b: number): number {
+  return a + b;
+}
+
+console.log('SUM =', sum(1,2));
